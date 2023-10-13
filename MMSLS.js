7595d7977fe0851c7971e14ba0fc2efd285887b1
jixingxing521 <101310153+jixingxing521@users.noreply.github.com>
2023-10-13 20:30:08 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=22885314&pid=ac7af1efa2de8c0def073c503af5d892&tid=c23aa5b6c57e5732c0e0ec59a851e3e2",
+    "https://www.maimemo.com/share/page?uid=22885314&pid=eeb8eace05d414328b617bdc1e01550c&tid=0b3ff5c3ac9955ee09c11278080d9d9d",
   ]
   /**
    * 生成随机数字
